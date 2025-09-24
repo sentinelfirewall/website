f5bfbf1048699138fc2292dd6f96cca2b90501ad
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-09-25 00:42:07 +0200

    Update GlobalDatabase.tsx

--- src/components/ui/GlobalDatabase.tsx
+++ src/components/ui/GlobalDatabase.tsx
@@ -41,17 +41,17 @@ export const GlobalDatabase: FunctionComponent = () => {
 
   const features = [
     {
-      name: "Global Clusters",
-      description: "Enable low-latency global access, enhancing performance.",
+      name: "Config Server Firewall",
+      description: "Comprehensive, straight-forward, and easy to configure SPI firewall.",
     },
     {
-      name: "Serverless Triggers",
-      description: "Trigger functions automatically for dynamic app behavior.",
+      name: "Login Failure Daemon",
+      description: "Constantly runs and periodically scans logs for brute-force detection.",
     },
     {
-      name: "Monitoring & Alerts",
+      name: "Control Panel Interface",
       description:
-        "Monitor health with key metrics or integrate third-party tools.",
+        "GUI accessible through cPanel, DirectAdmin and OpenPanel.",
     },
   ]
 
@@ -64,14 +64,14 @@ export const GlobalDatabase: FunctionComponent = () => {
         <div className="absolute top-[17rem] size-[40rem] rounded-full bg-indigo-800 blur-3xl md:top-[20rem]" />
         <div className="z-10 inline-block rounded-lg border border-indigo-400/20 bg-indigo-800/20 px-3 py-1.5 font-semibold uppercase leading-4 tracking-tight sm:text-sm">
           <span className="bg-gradient-to-b from-indigo-200 to-indigo-400 bg-clip-text text-transparent">
-            Made for the cloud
+            Supports major control panels
           </span>
         </div>
         <h2
           id="global-database-title"
           className="z-10 mt-6 inline-block bg-gradient-to-b from-white to-indigo-100 bg-clip-text px-2 text-center text-5xl font-bold tracking-tighter text-transparent md:text-8xl"
         >
-          The global <br /> cloud database
+          The firewall <br /> for webhosting
         </h2>
         <canvas
           className="absolute top-[7.1rem] z-20 aspect-square size-full max-w-fit md:top-[12rem]"
