c67457eac7b8f4d0360b17ec7673ab4cc8f5d647
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-09-25 00:54:03 +0200

    Update CodeExample.tsx

--- src/components/ui/CodeExample.tsx
+++ src/components/ui/CodeExample.tsx
@@ -89,16 +89,15 @@ export default function CodeExample() {
       aria-labelledby="code-example-title"
       className="mx-auto mt-28 w-full max-w-6xl px-3"
     >
-      <Badge>Developer-first</Badge>
+      <Badge>New</Badge>
       <h2
         id="code-example-title"
         className="mt-2 inline-block bg-gradient-to-br from-gray-900 to-gray-800 bg-clip-text py-2 text-4xl font-bold tracking-tighter text-transparent sm:text-6xl md:text-6xl dark:from-gray-50 dark:to-gray-300"
       >
-        Built by developers, <br /> for developers
+        Migrate from CSF <br /> to Sentinel Firewall
       </h2>
       <p className="mt-6 max-w-2xl text-lg text-gray-600 dark:text-gray-400">
-        Rich and expressive query language that allows you to filter and sort by
-        any field, no matter how nested it may be.
+        Sentinel is a maintained fork of the ConfigServer Firewall & Security (CSF) tool. Migrating from an existing CSF installation to Sentinel is simple and can be done with just a single command:
       </p>
       <CodeExampleTabs
         tab1={
